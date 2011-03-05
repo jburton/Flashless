d4e914dff95f1a55e0d4a01cd15c8b991a112c77
Christoph Petrick <xlii@gmx.de>
2011-03-05 20:36:19 +0800

    Support more YouTube source URL schemes

--- Plugin/Services/UCVideoServiceYoutube.m
+++ Plugin/Services/UCVideoServiceYoutube.m
@@ -56,6 +56,9 @@ OTHER DEALINGS IN THE SOFTWARE.
 		}
 		[[self class] scan:scanString from:@"/v/" to:@"&" into:&videoID];
 		if(videoID==nil) {
+			[[self class] scan:scanString from:@"/e/" to:@"&" into:&videoID];
+		}
+		if(videoID==nil) {
 			scanString = [self queryString];
 			if(scanString==nil) {
 				[self foundNoDownload];
@@ -64,11 +67,6 @@ OTHER DEALINGS IN THE SOFTWARE.
 			[[self class] scan:scanString from:@"v=" to:@"&" into:&videoID];
 		}
 		if(videoID==nil) {
-			scanString = [self queryString];
-			if(scanString==nil) {
-				[self foundNoDownload];
-				return;
-			}
 			[[self class] scan:scanString from:@"video_id=" to:@"&" into:&videoID];
 		}
 	}
